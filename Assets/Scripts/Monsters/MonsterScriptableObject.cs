6cb975fad6b9283152b0abac582c4535f514d9fd
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2021-02-15 20:40:30 -0500

    added npc sprites

---
+++ Assets/Scripts/Monsters/MonsterScriptableObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "MonsterData", menuName = "ScriptableObjects/MonsterScriptableObject", order = 1)]
+public class MonsterScriptableObject : ScriptableObject
+{
+    // current hp, current attack, current defense
+    public string Name;
+    public int HP;
+    public int Level;
+    public int XP;
+    public int Attack;
+    public int Defense;
+    public bool Fainted = false;
+    public Status CurrentStatus;
+   
+    public enum Status
+    {
+        Neutral,
+        Stunned,
+        Enraged,
+    }
+}
