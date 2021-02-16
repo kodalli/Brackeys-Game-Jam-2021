1fcfa796053e030b88b97671df520711272a5eb3
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2021-02-15 21:40:30 -0500

    Battle System

---
+++ Assets/Scripts/Battle System/BattleSystem.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState
+{
+    Begin,
+    PlayerTurn,
+    EnemyTurn,
+    Won,
+    Lost
+}
+
+public class BattleSystem : MonoBehaviour
+{
+    public BattleState state;
+
+    private void Start()
+    {
+        
+    }
+
+
+    IEnumerator SetupBattle()
+    {
+        yield return new WaitForSeconds(0f);
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        yield return new WaitForSeconds(0f);
+    }
+
+    IEnumerator PlayerHeal()
+    {
+        yield return new WaitForSeconds(0f);
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        yield return new WaitForSeconds(0f);
+    }
+
+    void EndBattle()
+    {
+
+    }
+
+    void PlayerTurn()
+    {
+
+    }
+
+    #region Buttons
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PlayerTurn)
+            return;
+
+        StartCoroutine(PlayerAttack());
+    }
+
+    public void OnHealButton()
+    {
+        if (state != BattleState.PlayerTurn)
+            return;
+
+        StartCoroutine(PlayerHeal());
+    }
+    #endregion
+}
