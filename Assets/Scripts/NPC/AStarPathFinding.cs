83a6a1db87c659373efe86d5158d26661c59ec30
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2021-02-23 01:44:55 -0500

    npc pathfinding

---
+++ Assets/Scripts/NPC/AStarPathFinding.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class AStarPathFinding
+{
+    public Tile tiletemp = new Tile();
+
+    public class Tile {
+        public Vector2Int location;
+        public int F;
+        public int G;
+        public int H;
+        public Tile parent;
+    }
+
+    public List<Vector2Int> FindPath(bool[,] grid, Vector2Int beg, Vector2Int end) {
+        bool[,] map = grid;
+        Tile start = new Tile { location = beg };
+        Tile target = new Tile { location = end };
+        List<Tile> openList = new List<Tile>();
+        List<Tile> closedList = new List<Tile>();
+        Tile current = null;
+        int g = 0;
+
+        openList.Add(start);
+
+        while (openList.Count > 0) {
+            int lowest = openList.Min(item => item.F);
+            current = openList.First(item => item.F == lowest);
+            closedList.Add(current);
+            openList.Remove(current);
+
+            if (closedList.FirstOrDefault(item => item.location == target.location) != null)
+                break;
+
+            List<Tile> adjSquares = GetWalkable(current, map);
+            g++;
+
+            foreach (var adjSq in adjSquares) {
+                if (closedList.FirstOrDefault(item => item.location == adjSq.location) != null)
+                    continue;
+
+                if (openList.FirstOrDefault(item => item.location == adjSq.location) == null) {
+                    adjSq.G = g;
+                    adjSq.H = HScore(adjSq.location, target.location);
+                    adjSq.F = adjSq.G + adjSq.H;
+                    adjSq.parent = current;
+                    openList.Insert(0, adjSq);
+                }
+                else if (g + adjSq.H < adjSq.F) {
+                    adjSq.G = g;
+                    adjSq.F = adjSq.G + adjSq.H;
+                    adjSq.parent = current;
+                }
+            }
+        }
+
+        List<Vector2Int> path = new List<Vector2Int>();
+
+        while (current != null) {
+            path.Add(current.location);
+            current = current.parent;
+        }
+
+        path.Reverse();
+
+        return path;
+    }
+
+    public static List<Tile> GetWalkable(Tile cur, bool[,] map) {
+        List<Tile> adj = new List<Tile>
+        {
+            new Tile { location = new Vector2Int(0, -1) + cur.location },
+            new Tile { location = new Vector2Int(0, 1) + cur.location },
+            new Tile { location = new Vector2Int(-1, 0) + cur.location },
+            new Tile { location = new Vector2Int(1, 0) + cur.location },
+        };
+
+        adj = adj.Where(item => item.location.x >= 0 && item.location.x < map.GetLength(0) && item.location.y >= 0 && item.location.y < map.GetLength(1)).ToList();
+
+        return adj.Where(item => map[item.location.x, item.location.y] == true).ToList();
+
+    }
+
+    public virtual int HScore(Vector2Int cur, Vector2Int target) {
+        return Mathf.Abs(cur.x - target.x) + Mathf.Abs(cur.y - target.y);
+    }
+
+}
---
+++ Assets/Scripts/NPC/NPCPath.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class NPCPath : MonoBehaviour
+{
+    [SerializeField] private Tilemap tileMap;
+    [SerializeField] private bool flipPath;
+
+    private List<Vector3> Path = new List<Vector3>();
+    private List<Vector3> WalkableTiles = new List<Vector3>();
+    private List<Vector2Int> WalkableIndexes = new List<Vector2Int>();
+
+    private void Start() {
+        tileMap.CompressBounds();
+        var path = FindPathInTilemapCoordinates();
+        Path = path;
+        StartCoroutine(WalkPath(path));
+    }
+
+    public List<Vector3> FindPathInTilemapCoordinates() {
+
+        Vector3[,] Points = new Vector3[tileMap.cellBounds.size.x, tileMap.cellBounds.size.y];
+        bool[,] Map = new bool[tileMap.cellBounds.size.x, tileMap.cellBounds.size.y];
+
+        WalkableTiles.Clear();
+        //List<Vector3> Path = new List<Vector3>();
+        for (int x = tileMap.cellBounds.xMin; x < tileMap.cellBounds.xMax; x++) {
+            for (int y = tileMap.cellBounds.yMin; y < tileMap.cellBounds.yMax; y++) {
+                Vector3Int localPlace = new Vector3Int(x, y, (int)tileMap.transform.position.y);
+                Vector3 worldPlace = tileMap.CellToWorld(localPlace);
+
+                int i = x - tileMap.cellBounds.xMin;
+                int j = y - tileMap.cellBounds.yMin;
+
+                if (tileMap.HasTile(localPlace)) {
+                    Points[i, j] = worldPlace;
+                    Map[i, j] = true;
+                    //Path.Add(worldPlace);
+                    WalkableTiles.Add(worldPlace);
+                    WalkableIndexes.Add(new Vector2Int(i, j));
+                    //Debug.Log(worldPlace);
+                } else { Map[i, j] = false; }
+
+            }
+        }
+
+        // get index of start and end position in Map
+
+        var res = GetStartEndPos(Map, WalkableIndexes);
+        var startIndex = res.Item1;
+        var endIndex = res.Item2;
+
+        if (flipPath) {
+            startIndex = res.Item2;
+            endIndex = res.Item1;
+        }
+
+        var astar = new AStarPathFinding();
+        var path = astar.FindPath(Map, startIndex, endIndex);
+        Debug.Log(path.Count);
+
+        Path.Clear();
+
+        foreach(var loc in path) {
+            Path.Add(Points[loc.x, loc.y]);
+        }
+
+        return Path;
+    }
+
+    IEnumerator WalkPath(List<Vector3> path) {
+        var rb = GetComponent<Rigidbody2D>();
+        foreach(var coord in path) {
+            //transform.position = coord;
+            //rb.MovePosition(transform.position + coord * Time.fixedDeltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, coord, 1f);
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+
+    (Vector2Int, Vector2Int) GetStartEndPos(bool[,] map, List<Vector2Int> walkable) {
+        var start = new Vector2Int();
+        var end = new Vector2Int();
+
+        var astar = new AStarPathFinding();
+        var tile = astar.tiletemp;
+
+        var count = 0;
+
+        foreach(var walk in walkable) {
+            tile.location = walk;
+            if (AStarPathFinding.GetWalkable(tile, map).Count < 2) {
+                if (count == 0) start = walk;
+                if (count == 1) end = walk;
+                count++;
+            }
+            if (count == 2) return (start, end);
+        }
+
+
+        return (start, end);
+    }
+}
