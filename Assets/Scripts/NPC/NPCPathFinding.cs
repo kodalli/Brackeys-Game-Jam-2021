4afb8cbf54a14212a3ca3c6c82c5eba158a0ec24
vazeraja <68131084+vazeraja@users.noreply.github.com>
2021-02-23 00:15:55 -0500

    npc nav mesh ai

---
+++ Assets/Scripts/NPC/NPCPathFinding.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCPathFinding : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
+    void Start(){
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        agent.SetDestination(target.position);
+    }
+}
