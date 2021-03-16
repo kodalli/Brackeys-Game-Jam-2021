6a1b1d538d3bcb8f2836c50113640435202e7971
vazeraja <68131084+vazeraja@users.noreply.github.com>
2021-03-15 23:04:29 -0400

    Platformer Asset and Clean Up
    Imported 3 versions of player controllers and cleaned up useless shit

---
+++ Assets/PlatformerControllerAssets/Scripts/StateMachineController/PlayerStates/SuperStates/PlayerGroundedState.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundedState : PlayerState {
+
+    protected int xInput;
+    private bool jumpInput;
+    private bool isGrounded;
+    public PlayerGroundedState(PlayerX player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName) {
+    }
+
+    public override void DoChecks() {
+        base.DoChecks();
+
+        isGrounded = player.CheckIfGrounded();
+    }
+
+    public override void Enter() {
+        base.Enter();
+
+        player.JumpState.ResetAmountOfJumpsLeft();
+    }
+
+    public override void Exit() {
+        base.Exit();
+    }
+
+    public override void LogicUpdate() {
+        base.LogicUpdate();
+
+        xInput = player.InputHandler.NormInputX;
+        jumpInput = player.InputHandler.JumpInput;
+
+        if (jumpInput && player.JumpState.canJump()) {
+            player.InputHandler.UseJumpInput();
+            stateMachine.ChangeState(player.JumpState);
+        } else if (!isGrounded) {
+            player.InAirState.StartCoyoteTime();
+            stateMachine.ChangeState(player.InAirState);
+        }
+    }
+
+    public override void PhysicsUpdate() {
+        base.PhysicsUpdate();
+    }
+}
