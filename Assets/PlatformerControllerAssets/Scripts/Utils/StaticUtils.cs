cc53a2594ca4b31a6bfe99966fc2990400344e06
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2021-05-01 15:57:35 -0400

    static utils update

--- Assets/PlatformerControllerAssets/Scripts/Utils/StaticUtils.cs
+++ Assets/PlatformerControllerAssets/Scripts/Utils/StaticUtils.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,13 +13,13 @@ using UnityEditor.Animations;
 /// Useful static functions
 /// </summary>
 namespace StaticUtils {
-    public static class StaticUtils {
+    internal static class StaticUtils {
 
 
         /// <summary>
         /// Returns the first gameabject in children with a specific name.
         /// </summary>
-        public static GameObject FindInChildren(this GameObject go, string name) {
+        internal static GameObject FindInChildren(this GameObject go, string name) {
             return (from x in go.GetComponentsInChildren<Transform>()
                     where Animator.StringToHash(x.gameObject.name) == Animator.StringToHash(name)
                     select x.gameObject).First();
@@ -28,15 +29,15 @@ namespace StaticUtils {
         /// <summary>
         /// Sets all the children gameobjects with a specific game component to true or false.
         /// </summary>
-        public static void SetActiveAllChildren<T>(this GameObject go, bool state) where T : UnityEngine.Component {
-            go.GetComponentsInChildren<T>().ToList().ForEach(x => x.gameObject.SetActive(state));
+        internal static void SetActiveAllChildren<T>(this GameObject go, bool state) where T : UnityEngine.Component {
+            // go.GetComponentsInChildren<T>().ToList().ForEach(x => x.gameObject.SetActive(state));
+            go.GetComponentsInChildren<T>().ForEach(x => x.gameObject.SetActive(false));
             go.SetActive(true);
         }
 
 
-        public static void DestroyAllChildren<T>(this GameObject go) where T : UnityEngine.Component {
-            go.GetComponentsInChildren<T>()
-                .ToList().ForEach(x => { if (Animator.StringToHash(x.name) != Animator.StringToHash(go.name)) { UnityEngine.Object.Destroy(x.gameObject); } });
+        internal static void DestroyAllChildren<T>(this GameObject go) where T : UnityEngine.Component {
+            go.GetComponentsInChildren<T>().ForEach(x => { if (Animator.StringToHash(x.name) != Animator.StringToHash(go.name)) { UnityEngine.Object.Destroy(x.gameObject); } });
         }
 
 
@@ -51,29 +52,33 @@ namespace StaticUtils {
         /// </code>
         /// </example>
         /// </summary>
-        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source) {
+        internal static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source) {
             return source.Select((item, index) => (item, index));
         }
 
         /// <summary>
         /// Returns all the state names from an animator as a string array.
         /// </summary>
-        public static AnimatorState[] GetStateNames(Animator animator) {
+        internal static AnimatorState[] GetStateNames(Animator animator) {
             AnimatorController controller = animator ? animator.runtimeAnimatorController as AnimatorController : null;
             return controller == null ? null : controller.layers.SelectMany(l => l.stateMachine.states).Select(s => s.state).ToArray();
         }
 
 
 
-        public static bool IsPlayingOnLayer(this Animator animator, int fullPathHash, int layer) {
+        internal static bool IsPlayingOnLayer(this Animator animator, int fullPathHash, int layer) {
             return animator.GetCurrentAnimatorStateInfo(layer).fullPathHash == fullPathHash;
         }
 
-        public static float NormalizedTime(this Animator animator, int layer) {
+        internal static float NormalizedTime(this Animator animator, int layer) {
             float time = animator.GetCurrentAnimatorStateInfo(layer).normalizedTime;
             return time > 1 ? 1 : time;
         }
 
+        internal static void ForEach<T>(this IEnumerable<T> source, Action<T> action) {
+            foreach (T item in source)
+                action(item);
+        }
 
     }
 }
